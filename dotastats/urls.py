4d7e2c01435097d6e057f52151726b2650575b4f
Roman Alexander <romanalexander@users.noreply.github.com>
2013-01-26 04:47:46 -0500

    Added in better handling for match_id.

--- dotastats/urls.py
+++ dotastats/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'dotastats.views.home', name='home'),
     url(r'^matches/$', 'dotastats.views.matches_overview', name='matches_overview'),
-    url(r'^matches/(?P<match_id>\d{8})/$', 'dotastats.views.matches_id', name='matches_id'),
+    url(r'^matches/(?P<match_id>\d+)/$', 'dotastats.views.matches_id', name='matches_id'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^steam/', include('django_openid_auth.urls')),
     url(r'^about/', 'dotastats.views.about', name='about'),
