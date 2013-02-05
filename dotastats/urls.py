817f50eb2fc53a0bcb98c799abd12445a08e9578
Roman Alexander <romanalexander@users.noreply.github.com>
2013-02-05 18:53:34 -0500

    Added static serving to url patterns. (for gunicorn)

--- dotastats/urls.py
+++ dotastats/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 dajaxice_autodiscover()
 admin.autodiscover()
 
@@ -18,4 +19,7 @@ urlpatterns = patterns('',
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 )
 
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
 urlpatterns += staticfiles_urlpatterns()
