4ca516f52ec03c416e2a40bb8a832f0b79321217
Roman Alexander <romanalexander@users.noreply.github.com>
2013-02-11 21:58:04 -0500

    Updated search system. Search system now uses server markup render instead of javascript client markup render.

--- dotastats/ajax.py
+++ dotastats/ajax.py
@@ -1,22 +1,30 @@
 from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
-
-# TODO: Use django-celery for parallelization.
+from django.template.loader import render_to_string
+from dotastats.models import Heroes
 
 @dajaxice_register
 def search_matches(request, search_request):
+    if len(search_request) < 2:
+        return None
     dajax = Dajax()
-    dajax.add_data("<tr><td>TestData</td></tr>", 'parse_matches')
+    dajax.add_data("<tr><td>TestData</td></tr>", 'render_matches')
     return dajax.json()
 
 @dajaxice_register
 def search_players(request, search_request):
+    if len(search_request) < 2:
+        return None
     dajax = Dajax()
-    dajax.add_data("<tr><td>TestData</td></tr>", 'parse_players')
+    dajax.add_data("<tr><td>TestData</td></tr>", 'render_players')
     return dajax.json()
 
 @dajaxice_register
 def search_heroes(request, search_request):
+    if len(search_request) < 2:
+        return None
     dajax = Dajax()
-    dajax.add_data("<tr><td>TestData</td></tr>", 'parse_heroes')
+    result_set = Heroes.objects.filter(dota2_name__icontains=search_request)[:25]
+    result_string = render_to_string('ajax/search_results_hero.html', dictionary=dict({'results': result_set}))    
+    dajax.add_data(result_string, 'render_heroes')
     return dajax.json()
--- dotastats/views.py
+++ dotastats/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.cache import cache_page
 def home(request):
     return render(request, 'home.html')
 
-@cache_page(60 * 15) # 15min
+@cache_page(60 * 1)
 def matches_overview(request):
     match_history_list = steamapi.GetLatestMatches()
     paginator = Paginator(match_history_list, 25)
@@ -21,26 +21,28 @@ def matches_overview(request):
     
     return render(request, 'match_history.html', {'match_history': match_history})
 
-@cache_page(60 * 60) # 60min
+@cache_page(60 * 5)
 def matches_id(request, match_id):
     result_match = steamapi.GetMatchDetails(match_id)
     return render(request, 'match_id.html', {'match': result_match, 'teams': (result_match.get_radiant_players(), result_match.get_dire_players())})
 
-@cache_page(60 * 60) # 60min
 def about(request):
     return render(request, 'about.html')
 
-@cache_page(60 * 60) # 60min
 def news(request):
     return render(request, 'news.html')
 
-@cache_page(60 * 60) # 60min
 def leagues(request):
     return render(request, 'leagues.html')
 
 # Ajax Polymorphic search provider. Consider using Haystack instead of internal.
 def search(request, search_param=None):
-    result_dict = dict({'error': None, 
+    error = None
+    if search_param == None:
+        search_param = ""
+    if len(search_param) < 2:
+        error = "Search query too short. Please add more keywords."
+    result_dict = dict({'error': error, 
                         'search_param': search_param})
     return render(request, 'search.html', result_dict)
 
